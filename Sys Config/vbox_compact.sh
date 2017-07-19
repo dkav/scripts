edc5d0eaa6f50d4ab3dbb3663fc4607a304e4bb5
Darren Kavanagh <dkav@users.noreply.github.com>
2017-07-18 23:25:26 -0700

    Add script to compress vdi
    Add script to compress VirtualBox virtual disks.

---
+++ Sys Config/vbox_compact.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Summary: Compact VirtualBox virtual disk
+#
+# Usage: vbox_compact <Virtual disk>
+
+vdisk=$1
+
+if [ -z "$vdisk" ]; then
+    echo "vbox_compact <Virtual disk>"
+else
+    VBoxManage modifymedium disk $vdisk --compact
+fi
